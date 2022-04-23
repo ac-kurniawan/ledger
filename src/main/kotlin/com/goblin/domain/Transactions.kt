773514380252c756946b8889f5e0e5afff503437
ac-kurniawan <ac.kurniawan99@gmail.com>
2022-04-23 15:14:39 +0700

    feat: update lint

--- src/main/kotlin/com/goblin/domain/Transactions.kt
+++ src/main/kotlin/com/goblin/domain/Transactions.kt
@@ -6,6 +6,7 @@ import java.time.Instant
 import java.util.UUID
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.EnumType
 import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -13,7 +14,6 @@ import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 import javax.persistence.Table
-import javax.persistence.EnumType
 
 @Entity
 @Table(name = "transactions", schema = "ledger")
